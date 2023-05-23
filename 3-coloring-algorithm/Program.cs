9b6467120a4a685543c8558d07ad8cf0c3585709
sosna <jakub.sosnowski2001@gmail.com>
2023-05-23 19:13:53 +0200

    Add project files.

---
+++ 3-coloring-algorithm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _3_coloring_algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
