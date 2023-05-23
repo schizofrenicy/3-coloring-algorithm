412bde4889ce3977a7f7067de371f0fb4c456d29
sosna <jakub.sosnowski2001@gmail.com>
2023-05-24 01:33:01 +0200

    Add naive 3-coloring algorithm
    * Install QuikGraph NuGet Package
    * Add function to check if graph is properly 3-colored

---
+++ 3-coloring-algorithm/NaiveAlgorithm.cs
@@ -0,0 +1,43 @@
+﻿using QuikGraph;
+
+namespace _3_coloring_algorithm
+{
+    static class NaiveAlgorithm
+    {
+        static readonly int numOfColors = 3;
+        static int[] colors = Array.Empty<int>();
+
+        public static bool Naive3Coloring(UndirectedGraph<int, SUndirectedEdge<int>> g, out int[] colorsResult)
+        {
+            colors = new int[g.VertexCount];
+
+            if (Naive3ColoringRecursive(g, 0))
+            {
+                colorsResult = colors;
+                return true;
+            }
+            else
+            {
+                colorsResult = Array.Empty<int>();
+                return false;
+            }
+        }
+
+        static bool Naive3ColoringRecursive(UndirectedGraph<int, SUndirectedEdge<int>> g, int vertex)
+        {
+            if (vertex == g.VertexCount)
+            {
+                return ProperColoringChecker.CheckColoring(g, colors);
+            }
+
+            for (int color = 0; color < numOfColors; color++)
+            {
+                colors[vertex] = color;
+
+                if (Naive3ColoringRecursive(g, vertex + 1)) return true;
+            }
+
+            return false;
+        }
+    }
+}
