5aa0884c516bda930e9416be804e78147d708eb1
H3w9VH3F4L4Tq5MbsfRi <116811873+H3w9VH3F4L4Tq5MbsfRi@users.noreply.github.com>
2023-06-05 21:50:54 +0200

    Implement methods needed in Lemma #2 (#5)

--- 3-coloring-algorithm/Program.cs
+++ 3-coloring-algorithm/Program.cs
@@ -1,6 +1,4 @@
-﻿using QuikGraph;
-
-namespace _3_coloring_algorithm
+﻿namespace _3_coloring_algorithm
 {
     internal class Program
     {
@@ -8,13 +6,13 @@ namespace _3_coloring_algorithm
         {
             // PONIZEJ MIERZENIE CZASU GENEROWANIA GRAFU O ZADANYM ROZMAIRZE, A POTEM ROZWIAZYWANIA TEGO GRAFU METODA BRUTE-FORCE
 
-            DateTime dateTime = DateTime.Now;
-            var g = GraphGenerator.Generate(20, 0.3);
-            Console.WriteLine(DateTime.Now - dateTime);
-            dateTime = DateTime.Now;
-            var success = NaiveAlgorithm.Naive3Coloring(g, out int[] colors);
-            Console.WriteLine(success);
-            Console.Write(DateTime.Now - dateTime);
+            //DateTime dateTime = DateTime.Now;
+            //var g = GraphGenerator.Generate(20, 0.3);
+            //Console.WriteLine(DateTime.Now - dateTime);
+            //dateTime = DateTime.Now;
+            //var success = NaiveAlgorithm.Naive3Coloring(g, out int[] colors);
+            //Console.WriteLine(success);
+            //Console.Write(DateTime.Now - dateTime);
 
             // PONIZEJ JAKIS PRZYKLADOWY MALY GRAF DO BADANIA CZY DZIALA
 
@@ -53,6 +51,29 @@ namespace _3_coloring_algorithm
             //        Console.WriteLine($"Vertex number: {i}, color number: {colors[i]}");
             //    }
             //}
+
+            var g = GraphGenerator.Generate(4, 100);
+
+            var csp = new _32CSP(g);
+            var vertices = csp.Vertices();
+            var vertexCount = csp.VertexCount();
+            var neighbors = csp.VertexNeighbors(0, (ColorEnum)0);
+            var neighbors2 = csp.VertexNeighbors(0, (ColorEnum)1);
+            var neighbors3 = csp.VertexNeighbors(1, (ColorEnum)0);
+            var removeEdge1 = csp.RemoveEdge((0, (ColorEnum)0), (1, (ColorEnum)0));
+            var neighbors4 = csp.VertexNeighbors(0, (ColorEnum)0);
+            var removeEdge2 = csp.RemoveEdge((0, (ColorEnum)0), (0, (ColorEnum)0));
+            var neighbors5 = csp.VertexNeighbors(0, (ColorEnum)0);
+            var removeEdge3 = csp.RemoveEdge((1, (ColorEnum)0), (0, (ColorEnum)0));
+            var neighbors6 = csp.VertexNeighbors(0, (ColorEnum)0);
+            var addEdge1 = csp.AddEdge((0, (ColorEnum)0), (1, (ColorEnum)0));
+            var neighbors7 = csp.VertexNeighbors(0, (ColorEnum)0);
+            var addEdge2 = csp.AddEdge((0, (ColorEnum)0), (0, (ColorEnum)0));
+            var neighbors8 = csp.VertexNeighbors(0, (ColorEnum)0);
+            var addEdge3 = csp.AddEdge((1, (ColorEnum)0), (0, (ColorEnum)0));
+            var neighbors9 = csp.VertexNeighbors(0, (ColorEnum)0);
+            var removeVertex1 = csp.RemoveVertex(0, (ColorEnum)0);
+            var neighbors10 = csp.VertexNeighbors(0, (ColorEnum)0);
         }
     }
 }
