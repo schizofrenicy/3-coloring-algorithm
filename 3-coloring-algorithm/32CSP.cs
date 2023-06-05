a58a477bd37111752b75a7ef7d9a90002ea92831
H3w9VH3F4L4Tq5MbsfRi <116811873+H3w9VH3F4L4Tq5MbsfRi@users.noreply.github.com>
2023-06-05 19:33:45 +0200

    Create 3,2-CSP sctructure and constructor (#3)

---
+++ 3-coloring-algorithm/32CSP.cs
@@ -0,0 +1,35 @@
+﻿using QuikGraph;
+
+namespace _3_coloring_algorithm
+{
+    internal class _32CSP
+    {
+        private UndirectedGraph<int, SUndirectedEdge<int>> g;
+        static readonly int a = 3;
+
+        public _32CSP(UndirectedGraph<int, SUndirectedEdge<int>> graph)
+        {
+            g = new();
+
+            for (int i = 0; i < g.VertexCount * a; i++)
+            {
+                g.AddVertex(i);
+            }
+
+            for (int i = 0; i < g.VertexCount; i++)
+            {
+                var vertices = g.AdjacentVertices(i);
+
+                foreach (var v in vertices)
+                {
+                    if (i < v)
+                    {
+                        g.AddEdge(new SUndirectedEdge<int>(i * a, v * a));
+                        g.AddEdge(new SUndirectedEdge<int>(i * a + 1, v * a + 1));
+                        g.AddEdge(new SUndirectedEdge<int>(i * a + 2, v * a + 2));
+                    }
+                }
+            }
+        }
+    }
+}
