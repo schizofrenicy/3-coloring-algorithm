bf5aed2889149974ec8d6c359e1eab850c4b79ab
sosna <jakub.sosnowski2001@gmail.com>
2023-06-05 18:37:39 +0200

    Add GraphGenerator.cs

---
+++ 3-coloring-algorithm/GraphGenerator.cs
@@ -0,0 +1,41 @@
+﻿using QuikGraph;
+
+namespace _3_coloring_algorithm
+{
+    static class GraphGenerator
+    {
+        static Random rnd = new();
+
+        public static UndirectedGraph<int, SUndirectedEdge<int>> Generate(int size, double percentage)
+        {
+            UndirectedGraph<int, SUndirectedEdge<int>> result = new();
+
+            for (int i = 0; i < size; i++)
+            {
+                result.AddVertex(i);
+            }
+
+            List<SUndirectedEdge<int>> possibleEdges = new();
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    possibleEdges.Add(new SUndirectedEdge<int>(i, j));
+                }
+            }
+
+            var edgesSize = percentage * possibleEdges.Count;
+
+            for (int i = 0; i < edgesSize; i++)
+            {
+                int idx = rnd.Next(possibleEdges.Count);
+                var edge = possibleEdges[idx];
+                possibleEdges.RemoveAt(idx);
+                result.AddEdge(edge);
+            }
+
+            return result;
+        }
+    }
+}
--- 3-coloring-algorithm/Program.cs
+++ 3-coloring-algorithm/Program.cs
@@ -6,41 +6,53 @@ namespace _3_coloring_algorithm
     {
         static void Main(string[] args)
         {
-            UndirectedGraph<int, SUndirectedEdge<int>> g = new();
-            for (int i = 0; i < 10; i++)
-            {
-                g.AddVertex(i);
-            }
+            // PONIZEJ MIERZENIE CZASU GENEROWANIA GRAFU O ZADANYM ROZMAIRZE, A POTEM ROZWIAZYWANIA TEGO GRAFU METODA BRUTE-FORCE
 
-            g.AddEdge(new SUndirectedEdge<int>(0, 1));
-            g.AddEdge(new SUndirectedEdge<int>(0, 4));
-            g.AddEdge(new SUndirectedEdge<int>(0, 5));
-            g.AddEdge(new SUndirectedEdge<int>(1, 2));
-            g.AddEdge(new SUndirectedEdge<int>(1, 6));
-            g.AddEdge(new SUndirectedEdge<int>(2, 3));
-            g.AddEdge(new SUndirectedEdge<int>(2, 7));
-            g.AddEdge(new SUndirectedEdge<int>(3, 4));
-            g.AddEdge(new SUndirectedEdge<int>(3, 8));
-            g.AddEdge(new SUndirectedEdge<int>(4, 9));
-            g.AddEdge(new SUndirectedEdge<int>(5, 7));
-            g.AddEdge(new SUndirectedEdge<int>(5, 8));
-            g.AddEdge(new SUndirectedEdge<int>(6, 8));
-            g.AddEdge(new SUndirectedEdge<int>(6, 9));
-            g.AddEdge(new SUndirectedEdge<int>(7, 9));
-            //g.AddEdge(new SUndirectedEdge<int>(0, 2));
-            //g.AddEdge(new SUndirectedEdge<int>(0, 3));
-            //g.AddEdge(new SUndirectedEdge<int>(1, 3));
-
+            DateTime dateTime = DateTime.Now;
+            var g = GraphGenerator.Generate(20, 0.3);
+            Console.WriteLine(DateTime.Now - dateTime);
+            dateTime = DateTime.Now;
             var success = NaiveAlgorithm.Naive3Coloring(g, out int[] colors);
             Console.WriteLine(success);
+            Console.Write(DateTime.Now - dateTime);
 
-            if (success)
-            {
-                for (int i = 0; i < g.VertexCount; i++)
-                {
-                    Console.WriteLine($"Vertex number: {i}, color number: {colors[i]}");
-                }
-            }
+            // PONIZEJ JAKIS PRZYKLADOWY MALY GRAF DO BADANIA CZY DZIALA
+
+            //UndirectedGraph<int, SUndirectedEdge<int>> g = new();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    g.AddVertex(i);
+            //}
+
+            //g.AddEdge(new SUndirectedEdge<int>(0, 1));
+            //g.AddEdge(new SUndirectedEdge<int>(0, 4));
+            //g.AddEdge(new SUndirectedEdge<int>(0, 5));
+            //g.AddEdge(new SUndirectedEdge<int>(1, 2));
+            //g.AddEdge(new SUndirectedEdge<int>(1, 6));
+            //g.AddEdge(new SUndirectedEdge<int>(2, 3));
+            //g.AddEdge(new SUndirectedEdge<int>(2, 7));
+            //g.AddEdge(new SUndirectedEdge<int>(3, 4));
+            //g.AddEdge(new SUndirectedEdge<int>(3, 8));
+            //g.AddEdge(new SUndirectedEdge<int>(4, 9));
+            //g.AddEdge(new SUndirectedEdge<int>(5, 7));
+            //g.AddEdge(new SUndirectedEdge<int>(5, 8));
+            //g.AddEdge(new SUndirectedEdge<int>(6, 8));
+            //g.AddEdge(new SUndirectedEdge<int>(6, 9));
+            //g.AddEdge(new SUndirectedEdge<int>(7, 9));
+            ////g.AddEdge(new SUndirectedEdge<int>(0, 2));
+            ////g.AddEdge(new SUndirectedEdge<int>(0, 3));
+            ////g.AddEdge(new SUndirectedEdge<int>(1, 3));
+
+            //var success = NaiveAlgorithm.Naive3Coloring(g, out int[] colors);
+            //Console.WriteLine(success);
+
+            //if (success)
+            //{
+            //    for (int i = 0; i < g.VertexCount; i++)
+            //    {
+            //        Console.WriteLine($"Vertex number: {i}, color number: {colors[i]}");
+            //    }
+            //}
         }
     }
 }
